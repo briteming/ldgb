3d3a893a6bd9376804c5c497b4fb4201b98bf5ad
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-05-31 21:44:58 +0100

    Add missing colours to the theme and improve the global focus style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,13 +8,15 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   :focus {
-    outline: 0;
-    box-shadow: 0 0 0 2px ${(props) => props.theme['blue']};
+    // Default: will be removed in Forced Color Modes
+    box-shadow: 0 0 0 1px ${(props) => props.theme['blue']};
+    // Fallback: will be visible with custom system colors in Forced Color Modes
+    outline: 1px solid transparent;
   }
 
   body {
-    background-color: ${(props) => props.theme['white']};
-    color: ${(props) => props.theme['blue']};
+    background-color: ${(props) => props.theme['base-background']};
+    color: ${(props) => props.theme['base-text']};
     -webkit-font-smoothing: antialised;
   }
 
