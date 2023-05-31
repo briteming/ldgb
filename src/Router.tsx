900ccd4c1261478a0363a365fb4e6f17462172f5
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-05-31 22:20:58 +0100

    Add main layout with shared header among pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home, Post, NotFound } from './pages'
+import { DefaultLayout } from './layouts'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/post/:issueNumber" element={<Post />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:issueNumber" element={<Post />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
   )
 }
