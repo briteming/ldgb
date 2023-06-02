263a8a3f8265ded4b580ec244045a76bf052711f
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-06-02 12:43:52 +0100

    Fetch profile data from Github API (axios setup)

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,43 +1,67 @@
+import { useEffect, useState } from 'react'
+import { api } from '../../lib/axios'
 import { Avatar, Bio, Container, Details, ExtraInfo, Name } from './styles'
+import { UserInfo } from './types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import {
   faBuilding,
   faUserGroup,
   faArrowUpRightFromSquare,
 } from '@fortawesome/free-solid-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export function Profile() {
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null)
+
+  async function getProfileData() {
+    const response = await api.get(
+      `/users/${import.meta.env.VITE_GITHUB_USERNAME}`
+    )
+
+    if (!response.data) return
+
+    setUserInfo({
+      url: response.data.html_url,
+      username: response.data.login,
+      name: response.data.name,
+      bio: response.data.bio,
+      followers: response.data.followers,
+      avatarUrl: response.data.avatar_url,
+    })
+  }
+
+  useEffect(() => {
+    getProfileData()
+  }, [])
+
   return (
     <Container>
-      <Avatar
-        src="https://github.com/leandrodalmolin.png"
-        alt="Profile image"
-      />
+      <Avatar src={userInfo?.avatarUrl} alt="Profile image" />
 
       <Details>
         <div>
           <header>
-            <Name>Leandro Dal Molin</Name>
-            <a href="https://www.github.com/leandrodalmolin" target="_blank">
+            <Name>{userInfo?.name}</Name>
+            <a href={userInfo?.url} target="_blank">
               Github
               <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="xs" />
             </a>
           </header>
-          <Bio>Frontend Software Engineer | Next.js, React, Javascript</Bio>
+          <Bio>{userInfo?.bio}</Bio>
         </div>
         <div>
           <ExtraInfo>
             <li>
               <FontAwesomeIcon icon={faGithub} />
-              leandrodalmolin
+              {userInfo?.username}
             </li>
             <li>
               <FontAwesomeIcon icon={faBuilding} />
               Great State
             </li>
             <li>
-              <FontAwesomeIcon icon={faUserGroup} />8 followers
+              <FontAwesomeIcon icon={faUserGroup} />
+              {userInfo?.followers} followers
             </li>
           </ExtraInfo>
         </div>
---
+++ src/components/Profile/types.ts
@@ -0,0 +1,8 @@
+export interface UserInfo {
+  url: string
+  username: string
+  name: string
+  bio: string
+  followers: number
+  avatarUrl: string
+}
