b93381cd3e6c0e06846841b8afcce9e0d5877a01
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2024-03-08 16:23:40 +0000

    Update env variables

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const GITHUB_API_BASE_URL = import.meta.env.VITE_GITHUB_API_BASE_URL
+
 export const api = axios.create({
-  baseURL: 'https://api.github.com',
+  baseURL: GITHUB_API_BASE_URL,
 })
