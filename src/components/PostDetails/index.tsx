6d7cd451b31355566219d3288802f5db4a468c79
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2024-03-11 12:41:14 +0000

    Fix loading skeleton display on single post

--- src/components/PostDetails/index.tsx
+++ src/components/PostDetails/index.tsx
@@ -15,43 +15,47 @@ import { ShadowCard, InlineList } from '@/components'
 export function PostDetails({ data, isLoading }: IPostDetails) {
   const navigate = useNavigate()
 
-  if (!data) return null
-  const { title, html_url, user, created_at, comments } = data
-  const relativeDate = dayjs(created_at).fromNow()
+  const renderContent = () => {
+    if (!data) return null
+    const { title, html_url, user, created_at, comments } = data
+    const relativeDate = dayjs(created_at).fromNow()
+
+    return (
+      <>
+        <header>
+          <button onClick={() => navigate(-1)}>
+            <FontAwesomeIcon icon={faChevronLeft} size="xs" />
+            Back
+          </button>
+          <a href={html_url} target="_blank">
+            See on Github
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="xs" />
+          </a>
+        </header>
+        <Title>{title}</Title>
+        <InlineList>
+          <li>
+            <FontAwesomeIcon icon={faGithub} />
+            {user?.login}
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faCalendarDay} />
+            {relativeDate}
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faComment} />
+            {comments} comments
+          </li>
+        </InlineList>
+      </>
+    )
+  }
 
   return (
     <ShadowCard isLoading={isLoading}>
       <Container>
         {isLoading && <p className="visually-hidden">Loading...</p>}
-        {!isLoading && (
-          <>
-            <header>
-              <button onClick={() => navigate(-1)}>
-                <FontAwesomeIcon icon={faChevronLeft} size="xs" />
-                Back
-              </button>
-              <a href={html_url} target="_blank">
-                See on Github
-                <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="xs" />
-              </a>
-            </header>
-            <Title>{title}</Title>
-            <InlineList>
-              <li>
-                <FontAwesomeIcon icon={faGithub} />
-                {user?.login}
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faCalendarDay} />
-                {relativeDate}
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faComment} />
-                {comments} comments
-              </li>
-            </InlineList>
-          </>
-        )}
+        {!isLoading && renderContent()}
       </Container>
     </ShadowCard>
   )
